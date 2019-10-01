e620bf3120d6f663962b0c9631fc66ffe41a2946
Sven Hertling <svenhertling@t-online.de>
2019-10-01 17:39:36 +0200

    updated test

--- matching-eval/src/test/java/de/uni_mannheim/informatik/dws/ontmatching/matchingeval/evaluator/util/PrefixLookupTest.java
+++ matching-eval/src/test/java/de/uni_mannheim/informatik/dws/ontmatching/matchingeval/evaluator/util/PrefixLookupTest.java
@@ -7,11 +7,11 @@ import org.junit.jupiter.api.Test;
 public class PrefixLookupTest {
     @Test
     void getPrefixedUri() {
-        assertEquals("protege:", PrefixLookup.getPrefix("http://protege.stanford.edu/plugins/owl/protege#"));
-        assertEquals("protege:Test", PrefixLookup.getPrefix("http://protege.stanford.edu/plugins/owl/protege#Test"));
-        assertEquals("owl:Class", PrefixLookup.getPrefix("http://www.w3.org/2002/07/owl#Class"));
-        assertEquals("http://my-ontology-uri.com/vocabulary#concept", PrefixLookup.getPrefix("http://my-ontology-uri.com/vocabulary#concept"));
-        assertEquals("http://my-ontology-uri.com/vocabulary/concept", PrefixLookup.getPrefix("http://my-ontology-uri.com/vocabulary/concept"));
+        assertEquals("protege:", PrefixLookup.DEFAULT.getPrefix("http://protege.stanford.edu/plugins/owl/protege#"));
+        assertEquals("protege:Test", PrefixLookup.DEFAULT.getPrefix("http://protege.stanford.edu/plugins/owl/protege#Test"));
+        assertEquals("owl:Class", PrefixLookup.DEFAULT.getPrefix("http://www.w3.org/2002/07/owl#Class"));
+        assertEquals("http://my-ontology-uri.com/vocabulary#concept", PrefixLookup.DEFAULT.getPrefix("http://my-ontology-uri.com/vocabulary#concept"));
+        assertEquals("http://my-ontology-uri.com/vocabulary/concept", PrefixLookup.DEFAULT.getPrefix("http://my-ontology-uri.com/vocabulary/concept"));
     }
 
     @Test
