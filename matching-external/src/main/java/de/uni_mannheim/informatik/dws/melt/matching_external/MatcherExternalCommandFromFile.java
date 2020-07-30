e6571d6e644b06ebbe4538fa44f0a65c00301544
Sven Hertling <svenhertling@t-online.de>
2020-07-30 09:02:21 +0200

    Updated external matcher with javadoc and moved methods to superclass

--- matching-external/src/main/java/de/uni_mannheim/informatik/dws/melt/matching_external/MatcherExternalCommandFromFile.java
+++ matching-external/src/main/java/de/uni_mannheim/informatik/dws/melt/matching_external/MatcherExternalCommandFromFile.java
@@ -1,7 +1,5 @@
 package de.uni_mannheim.informatik.dws.melt.matching_external;
 
-import java.io.File;
-import java.lang.management.ManagementFactory;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -11,22 +9,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Read the file "external/external_command.txt" and start an external process. The whole content of the file is used and newlines are ignored.
+ * The file can contain the following texts which are replaced.
+ * <ul>
+ * <li>"{File.separator}" with the os dependent file separator.On UNIX systems the value is <code>'/'</code>; on Microsoft Windows systems it is <code>'\\'</code>.</li>
+ * <li>"{File.pathSeparator}" with the os dependent path separator.It is used to separate filenames in a sequence of files given as a <em>path list</em>.On UNIX systems, this character is <code>':'</code>; on Microsoft Windows systems it is <code>';'</code>.</li>
+ * <li>"{xmx}" with the value xmx value the java process is started with (e.g. "{xmx}" is replaced with "-Xmx100G")</li>
+ * <li>"{xms}" with the value xms value the java process is started with (e.g. "{xms}" is replaced with "-Xms10G")</li>
+ * </ul>
+ * At the end of the command the URL of the source and target ontology are appended and optionally also the URL of the input alignment.
+ * The called process should access the arguments by index and not by name.
+ */
 public class MatcherExternalCommandFromFile extends MatcherExternal {
 
     protected Path filePath = Paths.get("external", "external_command.txt");
-    
-    protected static String xmx = "";
-    protected static String xms = "";
-    
-    static{
-        for(String s : ManagementFactory.getRuntimeMXBean().getInputArguments()){
-            if(s.toLowerCase().contains("xmx")){
-                xmx = s;
-            }else if(s.toLowerCase().contains("xms")){
-                xms = s;
-            }
-        }
-    }
     
     @Override
     protected List<String> getCommand(URL source, URL target, URL inputAlignment) throws Exception {
@@ -37,14 +34,5 @@ public class MatcherExternalCommandFromFile extends MatcherExternal {
         if(inputAlignment != null)
             command.add(inputAlignment.toString());
         return command;
-    }
-    
-    protected String replaceString(String s){
-        return s.replace("\r", "").replace("\n", "")
-                .replace("{File.pathSeparator}", File.pathSeparator)
-                .replace("{File.separator}", File.separator)
-                .replace("{xmx}", xmx)
-                .replace("{xms}", xms)
-                .trim();
     }
 }
--- matching-external/src/main/java/de/uni_mannheim/informatik/dws/melt/matching_external/MatcherExternalCommandFromLineFile.java
+++ matching-external/src/main/java/de/uni_mannheim/informatik/dws/melt/matching_external/MatcherExternalCommandFromLineFile.java
@@ -7,6 +7,18 @@ import static java.nio.file.Files.newBufferedReader;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Read the file "external/external_command.txt" and start an external process. Each line in the file represents an argument of the command line.
+ * The file can contain the following texts which are replaced.
+ * <ul>
+ * <li>"{File.separator}" with the os dependent file separator.On UNIX systems the value is <code>'/'</code>; on Microsoft Windows systems it is <code>'\\'</code>.</li>
+ * <li>"{File.pathSeparator}" with the os dependent path separator.It is used to separate filenames in a sequence of files given as a <em>path list</em>.On UNIX systems, this character is <code>':'</code>; on Microsoft Windows systems it is <code>';'</code>.</li>
+ * <li>"{xmx}" with the value xmx value the java process is started with (e.g. "{xmx}" is replaced with "-Xmx100G")</li>
+ * <li>"{xms}" with the value xms value the java process is started with (e.g. "{xms}" is replaced with "-Xms10G")</li>
+ * </ul>
+ * At the end of the command the URL of the source and target ontology are appended and optionally also the URL of the input alignment.
+ * The called process should access the arguments by index and not by name.
+ */
 public class MatcherExternalCommandFromLineFile extends MatcherExternalCommandFromFile {
     
     @Override
