1afe95608ae73576c16585d91a714ca509b71eec
janothan <jan.portisch@gmail.com>
2020-10-05 10:32:37 +0200

    Package info for matching-base.

---
+++ matching-base/src/main/java/de/uni_mannheim/informatik/dws/melt/matching_base/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The classes contained in this project can be used as simple template for ontology matchers.
+ * You can extend {@link de.uni_mannheim.informatik.dws.melt.matching_base.MatcherString} or
+ * {@link de.uni_mannheim.informatik.dws.melt.matching_base.MatcherURL} and implement {@code align} in order to get
+ * started.
+ */
+package de.uni_mannheim.informatik.dws.melt.matching_base;
