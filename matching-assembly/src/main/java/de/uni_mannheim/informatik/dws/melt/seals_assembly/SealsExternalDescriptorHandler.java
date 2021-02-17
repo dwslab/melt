a9d71c4164be5d5d35dab28847ef67e5207b0a5d
Sven Hertling <svenhertling@t-online.de>
2021-02-17 10:42:11 +0100

    update assembly

--- matching-assembly/src/main/java/de/uni_mannheim/informatik/dws/melt/seals_assembly/SealsExternalDescriptorHandler.java
+++ matching-assembly/src/main/java/de/uni_mannheim/informatik/dws/melt/seals_assembly/SealsExternalDescriptorHandler.java
@@ -1,16 +1,10 @@
 package de.uni_mannheim.informatik.dws.melt.seals_assembly;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-import org.apache.commons.compress.utils.IOUtils;
 import org.apache.maven.plugins.assembly.filter.ContainerDescriptorHandler;
 import org.codehaus.plexus.archiver.Archiver;
 import org.codehaus.plexus.archiver.ArchiverException;
@@ -20,14 +14,7 @@ import org.codehaus.plexus.component.annotations.Component;
 @Component(role = ContainerDescriptorHandler.class, hint = "sealsexternaldescriptorhandler")
 public class SealsExternalDescriptorHandler extends SealsDescriptorHandler {
     
-    //dummy matcher
-    protected String dummyMatcherMainclass = "de.uni_mannheim.informatik.dws.melt.matching_external.MatcherExternalCommandFromFile";
-    protected String dummyMatcherJarMain = "matching-external.jar";
-    protected String dummyMatcherJarDep = "matching-base.jar";
-    
-    //wrapper
-    protected String wrapperJar = "seals-external.jar";
-    protected String wrapperMainclass = "de.uni_mannheim.informatik.dws.melt.matching_seals_external.Main";
+    protected String wrapperMainclass = "";
     
     protected String externalCommand;
 
@@ -40,17 +27,17 @@ public class SealsExternalDescriptorHandler extends SealsDescriptorHandler {
             archiver.addFile(createStartStopFile("Stop"), "bin/stop.bat");
             archiver.addFile(createStartStopFile("Deploy"), "bin/deploy.bat");
             archiver.addFile(createStartStopFile("Undeploy"), "bin/undeploy.bat");            
-            archiver.addFile(createDescriptor(getLibEntries(), dummyMatcherMainclass, dummyMatcherJarMain), "descriptor.xml");
-            archiver.addFile(getFileFromResource(dummyMatcherJarMain), "bin/" + dummyMatcherJarMain);
-            archiver.addFile(getFileFromResource(dummyMatcherJarDep), "bin/lib/" + dummyMatcherJarDep);
-                       
+            archiver.addFile(createDescriptor(new ArrayList<>(), "de.uni_mannheim.informatik.dws.melt.matching_base.external.cli.MatcherCLIFromFile", "matching-base.jar"), "descriptor.xml");
+            archiver.addFile(getFileFromResource("matching-base.jar"), "bin/matching-base.jar");
+            
             if(existsProperty(this.externalCommand)){
                 archiver.addFile(getFileFromText(this.externalCommand), "conf/external/external_command.txt");
             }else if(existsProperty(this.mainclass)){
                 //the wrapper accepts as first argument the class which implements the matching interface
-                String wrapperExternalCommand = "java {xmx} -cp external{File.separator}" + wrapperJar + "{File.pathSeparator}external{File.separator}lib{File.separator}* " + this.wrapperMainclass + " " + this.mainclass;
+                String wrapperExternalCommand = "java ${Xmx} -cp external${file.separator}cli-receiver.jar${path.separator}external${file.separator}lib${file.separator}* de.uni_mannheim.informatik.dws.melt.receiver.cli.Main" +
+                        " -m " + this.mainclass + " -s ${source} -t ${target} $[-i ${inputAlignment}] $[-p ${parameters}]"; //arguments
                 archiver.addFile(getFileFromText(wrapperExternalCommand), "conf/external/external_command.txt");
-                archiver.addFile(getFileFromResource(wrapperJar), "conf/external/" + wrapperJar);
+                archiver.addFile(getFileFromResource("cli-receiver.jar"), "conf/external/cli-receiver.jar");
             }else{
                 throw new ArchiverException("You choose the external seals package but not providing external command nor mainclass.");
             }
@@ -60,48 +47,19 @@ public class SealsExternalDescriptorHandler extends SealsDescriptorHandler {
     }
     
     @Override
-    public List getVirtualFiles() {
+    public List<String> getVirtualFiles() {
         List<String> virtualFiles = new LinkedList<>(Arrays.asList(
                 "bin/start.bat", 
                 "bin/stop.bat", 
                 "bin/deploy.bat", 
                 "bin/undeploy.bat",
                 "descriptor.xml",
-                "bin/" + dummyMatcherJarMain,
-                "bin/lib/" + dummyMatcherJarDep,
+                "bin/matching-base.jar",
                 "conf/external/external_command.txt"));
         if(existsProperty(this.mainclass)){
-            virtualFiles.add("conf/external/lib/" + wrapperJar);
+            virtualFiles.add("conf/external/lib/cli-receiver.jar");
         }
         return virtualFiles;
-    }
-    
-    protected String getLibEntries(){
-        List<String> libList = new ArrayList<>();
-        libList.add("				<ns:lib>lib/" + dummyMatcherJarDep + "</ns:lib>");
-        return String.join(newline, libList);
-    }
-    
-    
-    protected File getFileFromText(String content) throws IOException{
-        File f = File.createTempFile("matching_file", ".txt");
-        f.deleteOnExit();
-        try (PrintWriter out = new PrintWriter(f)) {
-            out.println(content);
-        }
-        return f;
-    }
-    
-    
-    protected File getFileFromResource(String path) throws IOException{
-        File f = File.createTempFile("matching_file", ".jar");
-        f.deleteOnExit();
-        try (InputStream s = getClass().getClassLoader().getResourceAsStream(path)){
-            try (OutputStream outStream = new FileOutputStream(f)) {
-                IOUtils.copy(s, outStream);
-            }
-            return f;
-        }
     }
     
     protected boolean existsProperty(String prop){
