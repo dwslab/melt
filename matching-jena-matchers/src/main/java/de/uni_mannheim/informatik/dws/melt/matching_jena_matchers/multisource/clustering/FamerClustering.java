39a942915d44ce313158917d45bd533d83cff969
Sven Hertling <svenhertling@t-online.de>
2021-04-07 15:47:21 +0200

    updated clustering

--- matching-jena-matchers/src/main/java/de/uni_mannheim/informatik/dws/melt/matching_jena_matchers/multisource/clustering/FamerClustering.java
+++ matching-jena-matchers/src/main/java/de/uni_mannheim/informatik/dws/melt/matching_jena_matchers/multisource/clustering/FamerClustering.java
@@ -1,9 +1,12 @@
 package de.uni_mannheim.informatik.dws.melt.matching_jena_matchers.multisource.clustering;
 
+import de.uni_mannheim.informatik.dws.melt.matching_base.Filter;
 import de.uni_mannheim.informatik.dws.melt.matching_base.multisource.DatasetIDExtractor;
+import de.uni_mannheim.informatik.dws.melt.matching_base.multisource.IMatcherMultiSource;
 import de.uni_mannheim.informatik.dws.melt.yet_another_alignment_api.Alignment;
 import de.uni_mannheim.informatik.dws.melt.yet_another_alignment_api.Correspondence;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -26,14 +29,49 @@ import org.gradoop.flink.io.impl.graph.tuples.ImportEdge;
 import static org.gradoop.famer.clustering.common.PropertyNames.GRAPH_LABEL;
 import static org.gradoop.famer.clustering.common.PropertyNames.SIM_VALUE;
 import static org.gradoop.famer.clustering.common.PropertyNames.CLUSTER_ID;
+import org.gradoop.famer.clustering.parallelClustering.center.Center;
+import org.gradoop.famer.clustering.parallelClustering.clip.CLIP;
+import org.gradoop.famer.clustering.parallelClustering.clip.dataStructures.CLIPConfig;
+import org.gradoop.famer.clustering.parallelClustering.common.connectedComponents.ConnectedComponents;
+import org.gradoop.famer.clustering.parallelClustering.common.dataStructures.ClusteringOutputType;
+import org.gradoop.famer.clustering.parallelClustering.common.dataStructures.PrioritySelection;
+import org.gradoop.famer.clustering.parallelClustering.correlationClustering.CorrelationClustering;
+import org.gradoop.famer.clustering.parallelClustering.mergeCenter.MergeCenter;
+import org.gradoop.famer.clustering.parallelClustering.star.Star;
+import org.gradoop.famer.clustering.postprocessing.AbstractClusterPostprocessing;
 
 /**
- * 
- * https://git.informatik.uni-leipzig.de/dbs/FAMER/-/blob/master/famer-clustering/src/test/java/org/gradoop/famer/clustering/parallelClustering/center/CenterTest.java#L54
- * https://git.informatik.uni-leipzig.de/dbs/FAMER/-/blob/master/famer-preprocessing/src/main/java/org/gradoop/famer/preprocessing/io/benchmarks/abtbuy/AbtBuyReader.java
+ * A filter for multi source matching.
+ * It filters the input alignment by analyzing the structure of the correspondences.
+ * E.g. if many entities are fully connected, then this indicates that all of those correspondences are correct.
+ * More information on all possible algorithmn which should be chosen in the constructor can be found at <a href="https://dbs.uni-leipzig.de/file/eswc_0.pdf">Scalable Matching and Clustering of Entities with FAMER</a> .
+ * <a href="https://git.informatik.uni-leipzig.de/dbs/FAMER/-/tree/master/famer-clustering">The source code can be found at gitlab</a>.
  */
-public class FamerClustering {
+public class FamerClustering implements IMatcherMultiSource<Object, Alignment, Object>, Filter{
     private static final Logger LOGGER = LoggerFactory.getLogger(FamerClustering.class);
+    
+    private DatasetIDExtractor datsetIdExtractor;
+    private AbstractParallelClustering clusteringAlgorithm;
+
+    public FamerClustering(DatasetIDExtractor datsetIdExtractor, AbstractParallelClustering clusteringAlgorithm) {
+        this.datsetIdExtractor = datsetIdExtractor;
+        this.clusteringAlgorithm = clusteringAlgorithm;
+    }
+    
+    public FamerClustering(DatasetIDExtractor datsetIdExtractor) {
+        this.datsetIdExtractor = datsetIdExtractor;
+        this.clusteringAlgorithm = new Center(PrioritySelection.MIN, false, ClusteringOutputType.GRAPH, Integer.MAX_VALUE);
+    }
+    
+    @Override
+    public Alignment match(List<Object> models, Alignment inputAlignment, Object parameters) throws Exception {
+        return filter(inputAlignment);
+    }
+    
+    public Alignment filter(Alignment inputAlignment){
+        Map<String, Set<Long>> clusters = getClusters(inputAlignment, this.clusteringAlgorithm, this.datsetIdExtractor);
+        return RemoveCorrespondencesBasedOnClusterAssignments.removeCorrespondencesMultiCluster(inputAlignment, clusters);
+    }
     
     /**
      * Computes a map between uris and correspoding clusterId.
@@ -42,31 +80,105 @@ public class FamerClustering {
      * @param datsetIdExtractor the dataset id extractor to use. It gets an URI and returns the corresponding data source id.
      * @return a map between uris and correspoding clusterId
      */
-    public Map<String, Long> getClusters(Alignment alignment, AbstractParallelClustering clusteringAlgorithm, DatasetIDExtractor datsetIdExtractor){
-        LogicalGraph inputGraph = getLogicalGraphFromAlignment(alignment, datsetIdExtractor);
-        LogicalGraph clusteredGraph = clusteringAlgorithm.execute(inputGraph);
-        return getClusteringFromLogicalGraph(clusteredGraph);
-    }
-    
-    private Map<String, Long> getClusteringFromLogicalGraph(LogicalGraph clusteredGraph){
-        List<Tuple2<String, Long>> list = null;
-        try {
-            list = clusteredGraph.getVertices().map(
-                    vertex -> Tuple2.of(vertex.getLabel(), vertex.getPropertyValue(CLUSTER_ID).getLong())
-            ).returns(new TypeHint<Tuple2<String, Long>>() { }).collect();
-        } catch (Exception ex) {
-            LOGGER.error("Could not make the flink dataset to list", ex);
-            list = new ArrayList<>();
+    public static Map<String, Set<Long>> getClusters(Alignment alignment, AbstractParallelClustering clusteringAlgorithm, DatasetIDExtractor datsetIdExtractor){
+        if(clusteringAlgorithm == null){
+            LOGGER.warn("Clustering algorithmn is null. Thus no clustering can be executed. Returning empty map which can result in wrong filtering.");
+            return new HashMap<>();
+        }
+        LogicalGraphAndSourceIds l = getLogicalGraphFromAlignment(alignment, datsetIdExtractor);
+        LogicalGraph inputGraph = l.getGraph();
+        
+        if(clusteringAlgorithm instanceof CLIP){
+            //set the number of sources
+            CLIP c = (CLIP)clusteringAlgorithm;            
+            CLIPConfig clipConfig = new CLIPConfig(
+                    c.getClipConfig().getDelta(), 
+                    l.getIds().size(), //this value is changed.
+                    c.getClipConfig().isRemoveSourceConsistentVertices(),
+                    c.getClipConfig().getSimValueCoef(),
+                    c.getClipConfig().getDegreeCoef(),
+                    c.getClipConfig().getStrengthCoef()
+            );
+            clusteringAlgorithm = new CLIP(clipConfig, c.getClusteringOutputType(), c.getMaxIteration());
         }
         
-        Map<String, Long> vertexToClusterId = new HashMap<>();
-        for(Tuple2<String, Long> tuple : list){
-            vertexToClusterId.put(tuple.f0, tuple.f1);
+        LogicalGraph clusteredGraph = clusteringAlgorithm.execute(inputGraph);
+        
+        if(instanceOfOne(clusteringAlgorithm, Center.class, CorrelationClustering.class, MergeCenter.class)){
+            try {
+                return getClusteringFromLogicalGraphWithLong(clusteredGraph);
+            } catch (Exception ex) {
+                LOGGER.warn("Tried to extract cluster IDs from graph but did not work. Returning empty map which can result in wrong filtering.");
+                return new HashMap<>();
+            }
+        }
+        else if(instanceOfOne(clusteringAlgorithm, Star.class, ConnectedComponents.class)){
+            try {
+                return getClusteringFromLogicalGraphWithString(clusteredGraph);
+            } catch (Exception ex) {
+                LOGGER.warn("Tried to extract cluster IDs from graph but did not work. Returning empty map which can result in wrong filtering.");
+            }
+        }else{
+            LOGGER.info("The clusteringAlgorithm is not known and we try to extract the correct clusterIDs");
+            try {
+                return getClusteringFromLogicalGraphWithLong(clusteredGraph);
+            } catch (Exception ex) {
+                try {
+                    return getClusteringFromLogicalGraphWithString(clusteredGraph);
+                } catch (Exception e) {
+                    LOGGER.warn("Tried to extract cluster IDs from graph but did not work. Returning empty map which can result in wrong filtering.");
+                    
+                }
+            }
+        }
+        return new HashMap<>();
+    }
+    
+    public static boolean instanceOfOne(Object o, Class<?>... classes) {
+        for (Class<?> c : classes) {
+            if(c.isInstance(o)){
+                return true;
+            }
+        }
+        return false;
+    } 
+    
+    private static Map<String, Set<Long>> getClusteringFromLogicalGraphWithString(LogicalGraph clusteredGraph) throws Exception{
+        // https://git.informatik.uni-leipzig.de/dbs/FAMER/-/blob/master/famer-clustering/src/test/java/org/gradoop/famer/clustering/parallelClustering/star/StarTest.java#L137
+
+        List<Tuple2<String, String>> list = clusteredGraph.getVertices().map(
+                    vertex -> Tuple2.of(vertex.getLabel(), vertex.getPropertyValue(CLUSTER_ID).getString())
+            ).returns(new TypeHint<Tuple2<String, String>>() { }).collect();
+        
+        Map<String, Set<Long>> vertexToClusterId = new HashMap<>();
+        for(Tuple2<String, String> tuple : list){
+            for(String s : tuple.f1.split(",")){
+                try{
+                    long clusterId = Long.parseLong(s);
+                    vertexToClusterId.computeIfAbsent(tuple.f0, __-> new HashSet<>()).add(clusterId);
+                }catch(NumberFormatException ex){
+                    LOGGER.warn("Could not parse clusterID to Long: {} This mapping between vetrex and cluster id will be skipped. Be warned.", s);
+                }
+            }
         }
         return vertexToClusterId;
     }
     
-    private LogicalGraph getLogicalGraphFromAlignment(Alignment a, DatasetIDExtractor datsetIdExtractor){
+    private static Map<String, Set<Long>> getClusteringFromLogicalGraphWithLong(LogicalGraph clusteredGraph) throws Exception{
+        // https://git.informatik.uni-leipzig.de/dbs/FAMER/-/blob/master/famer-clustering/src/test/java/org/gradoop/famer/clustering/parallelClustering/center/CenterTest.java#L54
+        // https://git.informatik.uni-leipzig.de/dbs/FAMER/-/blob/master/famer-preprocessing/src/main/java/org/gradoop/famer/preprocessing/io/benchmarks/abtbuy/AbtBuyReader.java
+        List<Tuple2<String, Long>> list = clusteredGraph.getVertices().map(
+                    vertex -> Tuple2.of(vertex.getLabel(), vertex.getPropertyValue(CLUSTER_ID).getLong())
+            ).returns(new TypeHint<Tuple2<String, Long>>() { }).collect();
+        
+        Map<String, Set<Long>> vertexToClusterId = new HashMap<>();
+        for(Tuple2<String, Long> tuple : list){
+            vertexToClusterId.put(tuple.f0, new HashSet<>(Arrays.asList(tuple.f1)));
+        }
+        return vertexToClusterId;
+    }
+    
+    private static LogicalGraphAndSourceIds getLogicalGraphFromAlignment(Alignment a, DatasetIDExtractor datsetIdExtractor){
         Collection<ImportEdge<String>> edges = new ArrayList<>(a.size());
         Set<String> elements = new HashSet<>();
         long artificialEdgeId = 0;
@@ -78,16 +190,39 @@ public class FamerClustering {
             elements.add(c.getEntityTwo());
         }
         
+        Set<String> sourceIds = new HashSet<>();
         Collection<ImportVertex<String>> vertices = new ArrayList<>(elements.size());
         for(String element : elements){
             Properties properties = Properties.createWithCapacity(1);
-            properties.set(GRAPH_LABEL, PropertyValue.create(datsetIdExtractor.getDatasetID(element)));
+            String sourceId = datsetIdExtractor.getDatasetID(element);
+            sourceIds.add(sourceId);
+            properties.set(GRAPH_LABEL, PropertyValue.create(sourceId));
             vertices.add(new ImportVertex<>(element, element, properties));
         }
         
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
         GradoopFlinkConfig config = GradoopFlinkConfig.createConfig(env);
         
-        return new GraphDataSource<>(env.fromCollection(vertices), env.fromCollection(edges), null, config).getLogicalGraph();
+        return new LogicalGraphAndSourceIds(
+            new GraphDataSource<>(env.fromCollection(vertices), env.fromCollection(edges), null, config).getLogicalGraph(),
+            sourceIds
+        );
+    }
+}
+class LogicalGraphAndSourceIds{
+    private LogicalGraph graph;
+    private Set<String> ids;
+
+    public LogicalGraphAndSourceIds(LogicalGraph graph, Set<String> ids) {
+        this.graph = graph;
+        this.ids = ids;
+    }
+
+    public LogicalGraph getGraph() {
+        return graph;
+    }
+
+    public Set<String> getIds() {
+        return ids;
     }
 }
--- matching-jena-matchers/src/main/java/de/uni_mannheim/informatik/dws/melt/matching_jena_matchers/multisource/clustering/RemoveCorrespondencesBasedOnClusterAssignments.java
+++ matching-jena-matchers/src/main/java/de/uni_mannheim/informatik/dws/melt/matching_jena_matchers/multisource/clustering/RemoveCorrespondencesBasedOnClusterAssignments.java
@@ -4,8 +4,23 @@ import de.uni_mannheim.informatik.dws.melt.yet_another_alignment_api.Alignment;
 import de.uni_mannheim.informatik.dws.melt.yet_another_alignment_api.Correspondence;
 
 import java.util.Map;
+import java.util.Set;
 
+/**
+ * Helper calss for filtering correspondences based on cluster assignments.
+ * @author shertlin
+ */
 public class RemoveCorrespondencesBasedOnClusterAssignments {
+    
+    /**
+     * This function removes correspondences from the alignment where the two matched entities are not in the same cluster.
+     * It needs the mapping URI to cluster id.
+     * The provided alignment is not modified.
+     * @param <T> the type of the cluster ID
+     * @param alignment the alignemnt to filter
+     * @param uriToClusterId the mapping from uri to cluster id
+     * @return the filtered alignment
+     */
     public static <T> Alignment removeCorrespondences(Alignment alignment, Map<String, T> uriToClusterId){
         Alignment newAlignment = new Alignment(alignment, false);
         for(Correspondence c : alignment){
@@ -25,6 +40,41 @@ public class RemoveCorrespondencesBasedOnClusterAssignments {
             if(sourceClusterId.equals(targetClusterId)){
                 newAlignment.add(c);
             }//else we do nothing and do not add it to the resulting alignment.
+        }
+        return newAlignment;
+    }
+    
+    /**
+     * This function removes correspondences from the alignment where the two matched entities are not in the same cluster.
+     * It needs the mapping URI to set of cluster ids.
+     * The provided alignment is not modified.
+     * @param <T> the type of the cluster ID
+     * @param alignment the alignemnt to filter
+     * @param uriToClusterId the mapping from uri to set of cluster ids
+     * @return the filtered alignment
+     */
+    public static <T> Alignment removeCorrespondencesMultiCluster(Alignment alignment, Map<String, Set<T>> uriToClusterId){
+        Alignment newAlignment = new Alignment(alignment, false);
+        for(Correspondence c : alignment){
+            Set<T> sourceClusterIds = uriToClusterId.get(c.getEntityOne());
+            if(sourceClusterIds == null){
+                //if the clusterid is not known, then just keep the correspondence
+                newAlignment.add(c);
+                continue;
+            }
+            Set<T> targetClusterIds = uriToClusterId.get(c.getEntityTwo());
+            if(targetClusterIds == null){
+                //if the clusterid is not known, then just keep the correspondence
+                newAlignment.add(c);
+                continue;
+            }
+            
+            for(T sourceClusterId : sourceClusterIds){
+                if(targetClusterIds.contains(sourceClusterId)){
+                    newAlignment.add(c);
+                    break;
+                }
+            }
         }
         return newAlignment;
     }
