8683b0bb472f24821efa2ff2201a1ae5dc463b14
Sven Hertling <svenhertling@t-online.de>
2021-07-28 22:39:06 +0200

    added testcase for tracks

--- matching-data/src/test/java/de/uni_mannheim/informatik/dws/melt/matching_data/TrackTest.java
+++ matching-data/src/test/java/de/uni_mannheim/informatik/dws/melt/matching_data/TrackTest.java
@@ -1,6 +1,7 @@
 package de.uni_mannheim.informatik.dws.melt.matching_data;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -29,6 +30,30 @@ class TrackTest {
     }
     
     
+    @Test
+    public void testTrackOrderedAndUnique(){
+        assertEquals("cmt-conference" , TrackRepository.Conference.V1.getTestCase(0).getName());
+        assertEquals("cmt-confof" , TrackRepository.Conference.V1.getTestCase(1).getName());
+        assertEquals("cmt-edas" , TrackRepository.Conference.V1.getTestCase(2).getName());
+        assertEquals("cmt-ekaw" , TrackRepository.Conference.V1.getTestCase(3).getName());
+        assertEquals("cmt-iasted" , TrackRepository.Conference.V1.getTestCase(4).getName());
+        assertEquals("cmt-sigkdd" , TrackRepository.Conference.V1.getTestCase(5).getName());
+        assertEquals("conference-confof" , TrackRepository.Conference.V1.getTestCase(6).getName());
+        
+        //unique test cases:
+        
+        List<String> testCaseNames = new ArrayList<>();
+        for(TestCase tc : TrackRepository.Conference.V1.getTestCases()){
+            testCaseNames.add(tc.getName());
+        }
+        assertEquals(new HashSet<>(testCaseNames).size(), testCaseNames.size());
+        
+        testCaseNames = new ArrayList<>();
+        for(int i = 0; i < TrackRepository.Conference.V1.getTestCases().size(); i++){
+            testCaseNames.add(TrackRepository.Conference.V1.getTestCase(i).getName());
+        }
+        assertEquals(new HashSet<>(testCaseNames).size(), testCaseNames.size());        
+    }
     
     private String getName(URL url){
         String[] elements = url.toString().split("/");
