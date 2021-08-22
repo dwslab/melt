2e8a7989108910f2c83d21dca64bf4a0a960237d
Sven Hertling <svenhertling@t-online.de>
2021-08-22 21:14:46 +0200

    execute seals packaging only in java8

--- examples/simpleSealsMatcher/src/test/java/de/uni_mannheim/informatik/dws/melt/demomatcher/TestPackage.java
+++ examples/simpleSealsMatcher/src/test/java/de/uni_mannheim/informatik/dws/melt/demomatcher/TestPackage.java
@@ -5,6 +5,7 @@ import de.uni_mannheim.informatik.dws.melt.matching_data.TrackRepository;
 import de.uni_mannheim.informatik.dws.melt.matching_eval.ExecutionResult;
 import de.uni_mannheim.informatik.dws.melt.matching_eval.Executor;
 import java.io.File;
+import java.nio.file.Paths;
 
 /**
  * This class tests the generated seals package.
@@ -12,8 +13,10 @@ import java.io.File;
  * pipeline (CI) in github to test the package.
  */
 public class TestPackage {
-    public static void main(String[] args) {
-        MatcherSeals sealsMatcher = new MatcherSeals(new File("target/simpleSealsMatcher-1.0-seals_external.zip"));
+    public static void main(String[] args) {        
+        // cache seals client in CI (thus the location .m2 because it is cache already)
+        File sealsClient = Paths.get(System.getProperty("user.home"), ".m2", "seals_client.jar").toFile(); 
+        MatcherSeals sealsMatcher = new MatcherSeals(new File("target/simpleSealsMatcher-1.0-seals_external.zip"), sealsClient);
         ExecutionResult result = Executor.runSingle(TrackRepository.Anatomy.Default.getFirstTestCase(), sealsMatcher);
         
         if(result.getSystemAlignment().isEmpty()){
