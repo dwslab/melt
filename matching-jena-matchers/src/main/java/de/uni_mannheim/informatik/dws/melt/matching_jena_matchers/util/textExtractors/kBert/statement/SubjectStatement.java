563f463d5fb28aae7e44c21543a88d710dbe7e8f
Lukas <l.loos95@gmail.com>
2022-06-26 18:52:03 +0200

    feat(textExtractorKBert): extract json molecules

--- matching-jena-matchers/src/main/java/de/uni_mannheim/informatik/dws/melt/matching_jena_matchers/util/textExtractors/kBert/statement/SubjectStatement.java
+++ matching-jena-matchers/src/main/java/de/uni_mannheim/informatik/dws/melt/matching_jena_matchers/util/textExtractors/kBert/statement/SubjectStatement.java
@@ -5,25 +5,19 @@ import de.uni_mannheim.informatik.dws.melt.matching_jena_matchers.util.textExtra
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.Statement;
 
-import static de.uni_mannheim.informatik.dws.melt.matching_ml.python.nlptransformers.SentenceTransformersMatcher.NEWLINE;
-import static org.apache.commons.text.StringEscapeUtils.escapeCsv;
+import java.util.Map;
 
 public class SubjectStatement {
     protected final ProcessedProperty predicate;
     protected final ProcessedResource<Resource> subject;
-    protected final String target;
-    private final String objectId;
 
-    public SubjectStatement(Statement statement, String target) {
-        this.objectId = escapeCsv(statement.getObject().asResource().getURI());
-        this.target = target;
+    public SubjectStatement(Statement statement) {
         this.predicate = new ProcessedProperty(statement.getPredicate());
         this.subject = new ProcessedResource<>(statement.getSubject());
     }
 
-    public String getNormalized() {
-        return String.join(",", objectId, target, predicate.getNormalized(), subject.getNormalized(), "s")
-                + NEWLINE;
+    public Map<String, String> getNormalized() {
+        return Map.of("p", predicate.getNormalized(), "s", subject.getNormalized());
     }
 
     public ProcessedProperty getPredicate() {
