10ef56e9425c89d068216671732a26ba0e32038e
Lukas <l.loos95@gmail.com>
2022-07-06 14:06:00 +0200

    refactor(testPythonServer): use pytest fixture

--- matching-ml-python/matching_ml/test/test_python_server.py
+++ matching-ml-python/matching_ml/test/test_python_server.py
@@ -1,56 +1,40 @@
 # This unit test checks the python_server.py
 # Run `pytest` in the root directory of the jRDF2vec project (where the pom.xml resides).
 
-import threading
-import python_server_melt as server
-import time
-import requests
 from pathlib import Path
 
+import pytest
+import requests
 
-uri_prefix = "http://localhost:41193/"
-
-
-class ServerThread(threading.Thread):
-    def __init__(self, *args, **kwargs):
-        super(ServerThread, self).__init__(*args, **kwargs)
-        self._stop_event = threading.Event()
-        self.daemon = True
-
-    def run(self):
-        server.main()
-
-    def stop(self):
-        self._stop_event.set()
-        requests.get(uri_prefix + "shutdown")
-
-    def stopped(self):
-        return self._stop_event.is_set()
+from kbert.constants import RESOURCES_DIR, URI_PREFIX
+from kbert.test.ServerThread import ServerThread
+from python_server_melt import app as my_app
 
 
-server_thread = ServerThread()
-
-
-def setup_module(module):
-    """Let's start the server."""
-    wait_time_seconds = 10
+@pytest.fixture
+def server_thread():
+    server_thread = ServerThread()
+    # wait_time_seconds = 10
     server_thread.start()
-    print(f"Waiting {wait_time_seconds} seconds for the server to start.")
-    time.sleep(wait_time_seconds)
+    # print(f"Waiting {wait_time_seconds} seconds for the server to start.")
+    # time.sleep(wait_time_seconds)
+    yield
+    print("Shutting down...")
+    server_thread.stop()
 
 
-def test_get_vector():
+def test_get_vector(server_thread):
     test_model_vectors = "../../test/resources/test_model_vectors.kv"
     vector_test_path = Path(test_model_vectors)
     assert vector_test_path.is_file()
     result = requests.get(
-        uri_prefix + "get-vector",
+        URI_PREFIX + "get-vector",
         headers={"concept": "Europe", "vector_path": test_model_vectors},
     )
     assert len(result.content.decode("utf-8").split(" ")) == 100
 
 
-def test_is_in_vocabulary():
+def test_is_in_vocabulary(server_thread):
     test_model = "../../test/resources/test_model"
     test_vectors = "../../test/resources/test_model_vectors.kv"
     model_test_path = Path(test_model)
@@ -58,23 +42,23 @@ def test_is_in_vocabulary():
     assert model_test_path.is_file()
     assert vector_test_path.is_file()
     result = requests.get(
-        uri_prefix + "is-in-vocabulary",
+        URI_PREFIX + "is-in-vocabulary",
         headers={"concept": "Europe", "model_path": test_model},
     )
     assert result.content.decode("utf-8") == "True"
     result = requests.get(
-        uri_prefix + "is-in-vocabulary",
+        URI_PREFIX + "is-in-vocabulary",
         headers={"concept": "Europe", "vector_path": test_vectors},
     )
     assert result.content.decode("utf-8") == "True"
 
 
-def test_get_similarity():
+def test_get_similarity(server_thread):
     test_model = "../../test/resources/test_model"
     model_test_path = Path(test_model)
     assert model_test_path.is_file()
     result = requests.get(
-        uri_prefix + "get-similarity",
+        URI_PREFIX + "get-similarity",
         headers={
             "concept_1": "Europe",
             "concept_2": "united",
