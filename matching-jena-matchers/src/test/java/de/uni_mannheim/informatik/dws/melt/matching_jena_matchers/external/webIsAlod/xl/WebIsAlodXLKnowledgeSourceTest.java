2c7f11fdc37e82f88ea352a43046343a1cc15355
Sven Hertling <svenhertling@t-online.de>
2023-09-19 09:18:00 +0200

    disabled tests (see issue #215 )

--- matching-jena-matchers/src/test/java/de/uni_mannheim/informatik/dws/melt/matching_jena_matchers/external/webIsAlod/xl/WebIsAlodXLKnowledgeSourceTest.java
+++ matching-jena-matchers/src/test/java/de/uni_mannheim/informatik/dws/melt/matching_jena_matchers/external/webIsAlod/xl/WebIsAlodXLKnowledgeSourceTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Disabled;
 
+@Disabled("Disabled because bwcloud is offline.")
 class WebIsAlodXLKnowledgeSourceTest {
 
 
